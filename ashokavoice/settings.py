84cceff5aad7743e1345de0e531a362a297c71ba
aresly <ilianamacias67@gmail.com>
2024-06-08 01:12:29 -0500

    Commit 1

--- ashokavoice/settings.py
+++ ashokavoice/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ashokavoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'proyecto1',
+        'USER': 'postgres',
+        'PASSWORD': 'aresly27',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
